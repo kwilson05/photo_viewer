5b1fc8eee7f3acc722da8b80e78fa9281627e0a1
kasozi wilson <kasoziwilson@kasozis-MBP.lan>
2022-03-30 23:50:10 -0400

    remove comments'

--- src/ui/features/Home.js
+++ src/ui/features/Home.js
@@ -12,7 +12,7 @@ const s3 = new S3Client({
   region: REGION,
   credentials: fromCognitoIdentityPool({
     client: new CognitoIdentityClient({ region: REGION }),
-    identityPoolId: IDENTITY_POOL_ID, // IDENTITY_POOL_ID e.g., eu-west-1:xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx
+    identityPoolId: IDENTITY_POOL_ID,
   }),
 });
 
