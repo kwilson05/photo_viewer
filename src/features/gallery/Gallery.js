606e59be33c7cc7e6c0d25c575e5c2e0914a0a77
Kasozi Wilson <kwilson@lexile.com>
2022-04-27 15:48:55 -0400

    ugly back button

--- src/features/gallery/Gallery.js
+++ src/features/gallery/Gallery.js
@@ -1,3 +1,4 @@
+import { Link as ReactLink } from "react-router-dom";
 import styled, { css } from "styled-components";
 import { useParams } from "react-router-dom";
 import ImageGallery from "ui/components/ImageGallery";
@@ -6,7 +7,16 @@ const layout = css``;
 
 const Gallery = ({ className }) => {
   const { galleryName } = useParams();
-  return <ImageGallery className={className} gallery={galleryName} />;
+  return (
+    <>
+      <nav>
+        <ReactLink to="/">
+          <button type="button">Back</button>
+        </ReactLink>
+      </nav>
+      <ImageGallery className={className} gallery={galleryName} />
+    </>
+  );
 };
 
 export default styled(Gallery)`
